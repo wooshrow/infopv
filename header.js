b83b577ff093367d53b72d45a83131e33e5b2bf0
wooshrow <s.w.b.prasetya@uu.nl>
2025-08-27 13:29:45 +0200

    uploading website files

--- header.js
+++ header.js
@@ -2,19 +2,19 @@ var header =
      '<div class="row"> <!-- row 1 -->' +
      '<div class="six columns">' +
 	 '<img src="./images/PVpic.png" class="img-circle" height="30%" width="30%" >' +
-	  '<h5>Course Program Semantics & Verification 2024/25</h5>' +
+	  '<h5>Course Program Semantics & Verification 2025/26</h5>' +
       '</div>' +
       '<div id="nav" class="six columns">' +
       '  <a mystyle="navbutton2" href="index.html">news</a> &nbsp; ' +
-      '  <a mystyle="navbutton2" href="plan.html">course plan</a> &nbsp; ' +
-      '  <a mystyle="navbutton2" href="https://osiris-student.uu.nl/onderwijscatalogus/extern/cursus?cursuscode=INFOMPSV&collegejaar=2024">info @/education</a>  &nbsp;' +
+      '  <a mystyle="navbutton2" href="./plan.html">course plan</a> &nbsp; ' +
+      '  <a mystyle="navbutton2" href="https://osiris-student.uu.nl/onderwijscatalogus/extern/cursus?cursuscode=INFOMPSV&collegejaar=2025">info @osiris</a>  &nbsp;' +
       '  <a mystyle="navbutton2" href="javascript:history.go(0)">refresh</a>' +
       '</div>' +
     '</div> <!-- row 1 -->'
     +
     '<div class="row"> <!-- row 2 -->' +
       '  <div class="twelve columns">' +
-      '  <a class="button" href="material.html">Documentations</a>' +
+      '  <a class="button" href="./material.html">Documentations</a>' +
       '  <a class="button" href="https://www.spinroot.com">Spin MC</a>' +
 	  '  <a class="button" href="https://nusmv.fbk.eu/">NuSMV MC</a>' +
       '  <a class="button" href="https://www.cprover.org/cbmc/">CBMC MC</a>' +
